41fd21168ed94f2cf5eed8c0f5c7eaef5ecdebdf
JeskoFörster <foersterjesko@gmail.com>
2024-04-24 10:28:04 +0200

    added vorlesung 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,49 @@
 fun main() {
+    val hobbies:List<String> = listOf("Handball", "Fußball")
+    val susi = Person("Susi", "Sonne", 8, hobbies)
+    val lars = Person("Lars", "Lustig", -23, hobbies)
 
-    println("Entered Main!")
 
-    //take input
-    println("Please enter id of the program:")
-    val userInput: String = readln()
-    val number : Int = userInput.toInt()
+    println(lars)
+    lars.isAdult()
+    lars.has_hobby("Handball")
 
-    //decide what program to run
-    when(number) {
-        1 -> Test.main()
-        2 -> println("Program2")
-        else -> println("Unknown id. Please Enter a valid id.")
+}
+
+class Person(var vorname: String, var nachname:String, alter:Int = 0, var hobbies: List<String>){
+
+    var alter = alter
+        set(alter){
+            if(this.alter > alter) throw Exception("Yeet")
+            if(this.alter < 0) throw Exception("Yeet")
+            field = alter
+        }
+        get() = field
+
+    var volljährig: Boolean = false
+        get() = alter >= 18
+
+        set(value){
+            if(alter>18){
+                field = true
+                alter = 18
+            }
+        }
+
+
+    fun gibAlter() = alter
+
+    override fun toString(): String {
+        return "$vorname $nachname"
     }
 
-    println("Main Terminated!")
+    fun isAdult():Boolean{
+        return alter >= 18
+    }
+
+    fun isAdult2() = alter>= 18
+
+    fun has_hobby(potentialHobby:String):Boolean{
+        return hobbies.contains(potentialHobby)
+    }
 }
