8e7d6112113ea80775b90850c9f7a4e267e24f5b
JeskoFörster <foersterjesko@gmail.com>
2024-05-07 09:07:52 +0200

    started Blatt1.kt

--- src/main/kotlin/Blatt1.kt
+++ src/main/kotlin/Blatt1.kt
@@ -1,3 +1,47 @@
-class Blatt1 {
+import java.util.Date
 
+open class WorkUnit(){
+
+}
+class Task(val name: String, val decription:String, val deadline: Date, steps:MutableList<String>, estimatedTime:Int,var status: Status = Status.TODO) {
+
+    var steps = steps
+    var estimatedTime = estimatedTime
+        set(value) {field = if (value>0) value else throw Exception("time machine needed")}
+
+
+    fun addStep(step : String){
+        steps.add(step)
+    }
+}
+
+class Project(val name: String, val decription:String, val deadline: Date, var tasks: List<Task>, status: Status = Status.TODO) {
+    val progress: Double
+        get() {
+            var done: Int = 0
+            for(task in tasks){
+                if (task.status == Status.DONE)done++
+            }
+            return if (tasks.isNotEmpty()) done.toDouble() / tasks.size else 0.0
+        }
+}
+
+enum class Status{
+    TODO,
+    DONING,
+    DONE
+}
+
+fun main() {
+
+    val clean = Task("Bad säubern", "nervig", Date(), mutableListOf("Sachen holen", "putzen"), 80)
+    val clean2 = Task("Dachboden säubern", "weniger nervig", Date(), mutableListOf("Sachen holen", "Spinnen verschäuschen"), 80)
+    clean.estimatedTime = 70
+    clean.addStep("wegräumen")
+
+    val cleanProject = Project("clean", "Sauber machen", Date(), mutableListOf(clean, clean2))
+    println(cleanProject.tasks[0].steps[2])
+    println(cleanProject.progress)
+    cleanProject.tasks[0].status = Status.DONE
+    println(cleanProject.progress)
 }
