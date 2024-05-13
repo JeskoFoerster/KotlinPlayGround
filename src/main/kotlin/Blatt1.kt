8d76e5207d55c8797edc931da3cca663e2a5de04
JeskoFörster <foersterjesko@gmail.com>
2024-05-13 15:02:07 +0200

    finished Blatt1.kt

--- src/main/kotlin/Blatt1.kt
+++ src/main/kotlin/Blatt1.kt
@@ -1,21 +1,32 @@
 import java.util.Date
 
-open class WorkUnit(){
+open class WorkUnit(val name: String, val decription:String, val deadline: Date, var status: Status = Status.TODO){
+    fun changeStatus(status: Status){
+        this.status = status
+    }
 
+    open fun getSummary():String{
+        return "Die Aufgabe ’${name}’ mit der Beschreibung ’${decription}’ muss bis $deadline erledigt\n" +
+                "sein . Der aktuelle Status ist $status."
+    }
 }
-class Task(val name: String, val decription:String, val deadline: Date, steps:MutableList<String>, estimatedTime:Int,var status: Status = Status.TODO) {
+class Task( name: String, decription:String,  deadline: Date,  status: Status = Status.TODO, steps:MutableList<String>, estimatedTime:Int) : WorkUnit(name, decription, deadline, status){
 
     var steps = steps
     var estimatedTime = estimatedTime
         set(value) {field = if (value>0) value else throw Exception("time machine needed")}
 
-
     fun addStep(step : String){
         steps.add(step)
     }
+
+    override fun getSummary(): String {
+        return "${super.getSummary()} Die Arbeitsschritte sind: ${steps.joinToString(", ")}. Es wird voraussichtlich $estimatedTime Minuten dauern."
+    }
+
 }
 
-class Project(val name: String, val decription:String, val deadline: Date, var tasks: List<Task>, status: Status = Status.TODO) {
+class Project( name: String,  decription:String,  deadline: Date, var tasks: MutableList<Task>,  status: Status = Status.TODO) :WorkUnit(name, decription, deadline, status) {
     val progress: Double
         get() {
             var done: Int = 0
@@ -24,6 +35,18 @@ class Project(val name: String, val decription:String, val deadline: Date, var t
             }
             return if (tasks.isNotEmpty()) done.toDouble() / tasks.size else 0.0
         }
+
+
+    override fun getSummary(): String {
+        return "${super.getSummary()} Das Projekt beinhaltet ${tasks.size} Aufgaben. Aktuell sind ${progress * 100}% abgeschlossen."
+    }
+
+    fun addTask(task:Task){
+        if(task.deadline < deadline){
+            throw Exception("Deadline der Task darf nicht dach dem Projekt sein!")
+        }
+        tasks.add(task)
+    }
 }
 
 enum class Status{
@@ -34,8 +57,8 @@ enum class Status{
 
 fun main() {
 
-    val clean = Task("Bad säubern", "nervig", Date(), mutableListOf("Sachen holen", "putzen"), 80)
-    val clean2 = Task("Dachboden säubern", "weniger nervig", Date(), mutableListOf("Sachen holen", "Spinnen verschäuschen"), 80)
+    val clean = Task("Bad säubern", "nervig", Date(), status = Status.TODO, mutableListOf("Sachen holen", "putzen"), 80)
+    val clean2 = Task("Dachboden säubern", "weniger nervig", Date(), status = Status.TODO, mutableListOf("Sachen holen", "Spinnen verschäuschen"), 80)
     clean.estimatedTime = 70
     clean.addStep("wegräumen")
 
